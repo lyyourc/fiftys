198f70f9db4da11e3788546b760cd80f29ac8934
lyyourc <drakeleung42@gmail.com>
2019-01-17 01:27:08 +0800

    + todo status

--- src/components/status/index.tsx
+++ src/components/status/index.tsx
@@ -3,9 +3,10 @@ import { css } from '@emotion/core'
 import LoadingImage from '@/assets/images/loading.svg'
 import EmptyImage from '@/assets/images/empty.svg'
 import ErrorImage from '@/assets/images/error.svg'
+import TodoImage from '@/assets/images/todo.svg'
 
 interface StatusProps {
-  status: 'loading' | 'empty' | 'error'
+  status: 'loading' | 'empty' | 'error' | 'todo'
 }
 
 type StatusImageMap = { [key in StatusProps['status']]: string }
@@ -17,6 +18,7 @@ export default function Status(props: StatusProps) {
     empty: EmptyImage,
     error: ErrorImage,
     loading: LoadingImage,
+    todo: TodoImage,
   }
 
   return (
--- src/pages/fqa/index.tsx
+++ src/pages/fqa/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { css } from '@emotion/core'
+import Status from '@/components/status'
+import { Theme } from '@/styled/theme'
 
 export default function FqaPage() {
-  return <div>fqa</div>
+  return (
+    <div
+      css={(theme: Theme) => css`
+        height: calc(100vh - ${theme.heights.navbar});
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <Status status="todo" />
+    </div>
+  )
 }
